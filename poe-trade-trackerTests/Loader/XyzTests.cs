7f2a935916a2c4d5a2088aad6d6a2925564f9484
phxswjjj <phxswjjj@gmail.com>
2018-04-04 01:55:27 +0800

    add xyz test

--- poe-trade-trackerTests/Loader/XyzTests.cs
+++ poe-trade-trackerTests/Loader/XyzTests.cs
@@ -12,13 +12,30 @@ namespace POE.Loader.Tests
     public class XyzTests
     {
         [TestMethod()]
-        public void CreateFound()
+        public void Create_SiteNotFound()
+        {
+            Assert.ThrowsException<System.Net.WebException>(() =>
+            {
+                Xyz.Create("http://poedb1.tw/xyz.php?name=%E5%AD%B8%E5%AF%8C%E4%B9%8B%E7%AD%86&league=Warbands&status=1&boi=9&boc_min=1&boc_max=3");
+            });
+        }
+        [TestMethod()]
+        public void Create_FormatInvalid()
+        {
+            var xyz = Xyz.Create("http://poedb.tw/");
+            Assert.IsFalse(xyz.IsFoundItems);
+        }
+        [TestMethod()]
+        public void Create_Found()
         {
             var xyz = Xyz.Create("http://poedb.tw/xyz.php?name=%E5%AD%B8%E5%AF%8C%E4%B9%8B%E7%AD%86&league=Warbands&status=1&boi=9&boc_min=1&boc_max=3");
             Assert.IsTrue(xyz.IsFoundItems);
+
+            Assert.AreEqual("學富之筆 粗紋法杖", xyz.Items.First().Name);
+            Assert.AreEqual("崇高石", xyz.Items.First().PriceUnit);
         }
         [TestMethod()]
-        public void CreateNotFound()
+        public void Create_NotFound()
         {
             var xyz = Xyz.Create("http://poedb.tw/xyz.php?name=%E5%AD%B8%E5%AF%8C%E4%B9%8B%E7%AD%86&league=Warbands&status=1&boi=9&boc_min=1&boc_max=0");
             Assert.IsFalse(xyz.IsFoundItems);
